735949fc04f5263563a83e90bc2a6778d8e85189
SaintSanta <aleks2art@gmail.com>
2022-02-13 23:51:10 +0300

    add: const

---
+++ src/constants/benefits.js
@@ -0,0 +1,27 @@
+import iconTruck from '../components/main-blocks/benefits/assets/IconTruck';
+import iconRefresh from '../components/main-blocks/benefits/assets/IconRefresh';
+import iconSupport from '../components/main-blocks/benefits/assets/IconSupport';
+
+export const BENEFITS = [
+    {
+        id: '1',
+        title: 'FREE SHIPPING',
+        text: 'On all UA order or order above $100',
+        imageSrc: iconTruck,
+        name: 'Truck',
+      },
+      {
+        id: '2',
+        title: '30 DAYS RETURN',
+        text: 'Simply return it within 30 days for an exchange',
+        imageSrc: iconRefresh,
+        name: 'Refresh',
+      },
+      {
+        id: '3',
+        title: 'SUPPORT 24/7',
+        text: 'Contact us 24 hours a day, 7 days a week',
+        imageSrc: iconSupport,
+        name: 'Support',
+      },
+];
---
+++ src/constants/menu.js
@@ -0,0 +1,30 @@
+export const MENU = [
+    {
+        id: '1',
+        name: 'About Us'
+    },
+    {
+        id: '2',
+        name: 'Women'
+    },
+    {
+        id: '3',
+        name: 'Men'
+    },
+    {
+        id: '4',
+        name: 'Beauty'
+    },
+    {
+        id: '5',
+        name: 'Accessories'
+    },
+    {
+        id: '6',
+        name: 'Blog'
+    },
+    {
+        id: '7',
+        name: 'Contact'
+    },
+]
