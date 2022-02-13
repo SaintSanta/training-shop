fc301a03f2d357d5c952d3fd715f2c81919fe0f0
SaintSanta <aleks2art@gmail.com>
2022-02-14 00:25:38 +0300

    add: const

---
+++ src/constants/nav-bar-icons.js
@@ -0,0 +1,27 @@
+import iconSearch from '../components/header/nav-bar/assets/IconSearch';
+import iconGlobe from '../components/header/nav-bar/assets/IconGlobe';
+import iconUser from '../components/header/nav-bar/assets/IconUser';
+import iconBasket from '../components/header/nav-bar/assets/IconBasket';
+
+export const NAV_BAR_ICONS = [
+    {
+        id: '1',
+        name: 'Search',
+        imageSrc: iconSearch,
+    },
+    {
+        id: '2',
+        name: 'Globe Language',
+        imageSrc: iconGlobe,
+    },
+    {
+        id: '3',
+        name: 'User Account',
+        imageSrc: iconUser,
+    },
+    {
+        id: '4',
+        name: 'Basket',
+        imageSrc: iconBasket,
+    },
+]
---
+++ src/constants/social-networks.js
@@ -0,0 +1,31 @@
+import iconFacebook from '../components/header/contacts/assets/IconFacebook';
+import iconTwitter from '../components/header/contacts/assets/iconTwitter';
+import iconInstagram from '../components/header/contacts/assets/iconInstagram';
+import iconPinterest from '../components/header/contacts/assets/iconPinterest';
+
+export const SOCIAL_NETWORKS = [
+    {
+        id: '1',
+        name: 'Facebook',
+        imageSrc: iconFacebook,
+        href: '#',
+    },
+    {
+        id: '2',
+        name: 'Twitter',
+        imageSrc: iconTwitter,
+        href: '#',
+    },
+    {
+        id: '1',
+        name: 'Instagram',
+        imageSrc: iconInstagram,
+        href: '#',
+    },
+    {
+        id: '1',
+        name: 'Pinterest',
+        imageSrc: iconPinterest,
+        href: '#',
+    },
+];
