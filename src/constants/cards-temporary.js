358080415a544e6a7fad27086725cb7a99545926
SaintSanta <aleks2art@gmail.com>
2022-02-14 02:42:28 +0300

    add: const

---
+++ src/constants/cards-temporary.js
@@ -0,0 +1,136 @@
+import bluePants from '../components/cards-item/assets/bluePants';
+import mentolT_Shirt from '../components/cards-item/assets/mentolT_Shirt';
+import whiteBlouse from '../components/cards-item/assets/whiteBlouse';
+import whitePants from '../components/cards-item/assets/whitePants';
+import GreySweatshirt from '../components/cards-item/assets/GreySweatshirt';
+import blackPants from '../components/cards-item/assets/blackPants';
+import whiteBreeches from '../components/cards-item/assets/whiteBreeches';
+import whiteT_Shirt from '../components/cards-item/assets/whiteT_Shirt';
+
+import creamSweatshirt from '../components/cards-item/assets/creamSweatshirt';
+import blueManPants from '../components/cards-item/assets/blueManPants';
+import greySweatshirt from '../components/cards-item/assets/GreySweatshirt';
+import cap from '../components/cards-item/assets/cap';
+import creamPullover from '../components/cards-item/assets/creamPullover';
+import blueShirt from '../components/cards-item/assets/blueShirt';
+import greyPants from '../components/cards-item/assets/greyPants';
+import greyManPants from '../components/cards-item/assets/GreyManPants';
+
+export const CARDS_TEMPORARY = {
+    women: [
+        {
+            id: '1',
+            imageSrc: bluePants,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '2',
+            imageSrc: mentolT_Shirt,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '3',
+            imageSrc: whiteBlouse,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '4',
+            imageSrc: whitePants,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '5',
+            imageSrc: GreySweatshirt,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '6',
+            imageSrc: blackPants,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '7',
+            imageSrc: whiteBreeches,
+            name: "Women's tracksuit Q109",
+            price: '38.00',
+            rating: 4, 
+        },
+        {
+            id: '8',
+            imageSrc: whiteT_Shirt,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+    ],
+    men: [
+        {
+            id: '1',
+            imageSrc: creamSweatshirt,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '2',
+            imageSrc: blueManPants,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '3',
+            imageSrc: greySweatshirt,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '4',
+            imageSrc: cap,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '5',
+            imageSrc: creamPullover,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '6',
+            imageSrc: blueShirt,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+        {
+            id: '7',
+            imageSrc: greyPants,
+            name: "Women's tracksuit Q109",
+            price: '38.00',
+            rating: 4, 
+        },
+        {
+            id: '8',
+            imageSrc: greyManPants,
+            name: "Women's tracksuit Q109",
+            price: '30.00',
+            rating: 4, 
+        },
+    ],
+};
---
+++ src/constants/footer-nav.js
@@ -0,0 +1,77 @@
+export const FOOTER_NAV = [
+    {
+        id: '1',
+        links: [
+        {
+            data: 'CATEGORIES',
+            href: '#',
+        },
+        {
+            data: 'Men',
+            href: '#',
+        },
+        {
+            data: 'Women',
+            href: '#',
+        },
+        {
+            data: 'Accessories',
+            href: '#',
+        },
+        {
+            data: 'Beauty',
+            href: '#',
+        },
+        ],
+    },
+    {
+        id: '2',
+        links: [
+        {
+            data: 'INFORMATION',
+            href: '#',
+        },
+        {
+            data: 'About Us',
+            href: '#',
+        },
+        {
+            data: 'Contact Us',
+            href: '#',
+        },
+        {
+            data: 'Blog',
+            href: '#',
+        },
+        {
+            data: 'FAQs',
+            href: '#',
+        },
+        ],
+    },
+    {
+        id: '3',
+        links: [
+        {
+            data: 'USEFUL LINKS',
+            href: '#',
+        },
+        {
+            data: 'Terms & Conditions',
+            href: '#',
+        },
+        {
+            data: 'Returns & Exchanges',
+            href: '#',
+        },
+        {
+            data: 'Shipping & Delivery',
+            href: '#',
+        },
+        {
+            data: 'Privacy Policy',
+            href: '#',
+        },
+        ],
+    },
+];
